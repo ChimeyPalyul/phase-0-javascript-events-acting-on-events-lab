ace40ecda083c5048c7561e02c7bd8a37447afcc
Chimey Palyul <palyul.chimey@gmail.com>
2023-09-13 16:03:14 -0600

    ArrowLeft/ArrowRight/keydownEventListeners

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-// Your code here
-const dodger = document.getElementById("dodger");
-function moveDodgerLeft() {
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4";
+
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
+    if(left > 0){
+    dodger.style.left = `${left - 1}px`;
     }
   }
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
-function moveDodgerRight(){
+})
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function(e){
+  if(e.key === "ArrowLeft"){
+    moveDodgerLeft();
+  }
+})
+
+document.addEventListener('keydown',function(event){
+  if(event.key === 'ArrowRight'){
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-    
-    if (left < 180){
-        dodger.style.left = `${left + 1}px`;
+    if(left < 360){
+    dodger.style.left = `${left + 1}px`;
     }
-    document.addEventListener("keydown", function (e){
-        if (e.key === "ArrowRight"){
-            moveDodgerRight();
-        }
-    }
-    )
-};
+  }
+});
 
+function moveDodgerRight(){
+  const leftNumbers = dodger.style.left.replace('px','');
+  const left = parseInt(leftNumbers,10);
+  if(left < 360){
+    dodger.style.left = `${left+ 1}px`;
+  }
+}
+
+document.addEventListener(keydown,function(e){
+  if(e.key === 'ArrowRight'){
+    moveDodgerRight();
+  }
+
+
+})
